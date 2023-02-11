172d5b36ad86c42e4eadaca54a208524da1e0ac1
Ilya Antonov <ilya.anton0v@vk.com>
2023-02-11 10:40:46 +0300

    add comments

--- SetLab1_2_Antonov.cpp
+++ SetLab1_2_Antonov.cpp
@@ -1,13 +1,16 @@
 #include "SetLab1_2_Antonov.h"
 
+//  Создание пустого множества
 Set* createEmptySet() {
     return new Set();
 }
 
+//  Проверка на пустоту множества
 bool isEmptySet(Set* first) {
     return first == nullptr ? true : false;
 }
 
+//  Проверка элемента на принадлежность множеству
 bool isSetHasElement(Set* first, int element) {
     if (isEmptySet(first) == true) {
         return false;
--- SetLab1_2_Antonov.h
+++ SetLab1_2_Antonov.h
@@ -7,18 +7,27 @@ struct Set {
     Set* next;
 };
 
+//  Создание пустого множества
 Set* createEmptySet() {}
 
+//  Проверка на пустоту множества
 bool isEmptySet(Set* first) {}
 
+//  Проверка элемента на принадлежность множеству
 bool isSetHasElement(Set* first, int element) {}
 
+//  Добавление нового элемента в начало множества
 Set* addNewElement(Set* first, int element) {}
 
+//  Создание множества по заданным параметрам, проверяя возможность
+//  создания множества
 Set* createNewSet(int quantity, int min, int max) {}
 
+//  Мощность множества
 int setPowers(Set* first) {}
 
+//  Вывод элементов множества
 string setView(Set* first, string separator) {}
 
+//  Удаление множества (очистка занимаемой множеством памяти)
 Set* deleteSet(Set* first) {}
