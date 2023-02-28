0cc58fdcaa83e54a92f36bc8ff1a6c00de9356d4
Ilya Antonov <ilya.anton0v@vk.com>
2023-02-28 22:12:31 +0300

    add setlocale

--- Lab1_2_Antonov.cpp
+++ Lab1_2_Antonov.cpp
@@ -1,6 +1,7 @@
 #include "SetLab1_2_Antonov.h"
 
 int main(int argc, char **argv){
+    setlocale(LC_ALL, "Russian");
     Set* mySet = createNewSet(6, 0, 10);
     string setElements = setView(mySet, *" ");
     cout << "Элементы: " << setElements << endl;
